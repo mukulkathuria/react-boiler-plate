515fd590569179572a134d05bfcfcda2f9d0c669
Mukul Kathuria <mukulkathuria008@yahoo.com>
2020-09-06 19:41:46 +0530

    Initialize BoilerPlate with basic Webpack Configuration

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { MyDiv } from "./app.style";
+
+const App = () => {
+  return (
+    <MyDiv>
+      <p>App Testing</p>
+    </MyDiv>
+  );
+};
+export default App;
